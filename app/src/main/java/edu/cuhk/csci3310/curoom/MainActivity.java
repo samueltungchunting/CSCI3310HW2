1dd7b29923d9169fba29d7cd5af87d0ebc93b358
Sam <vyan452000@gmail.com>
2024-04-11 12:51:30 +0800

    fetching and parsing json file & display classrm img

--- app/src/main/java/edu/cuhk/csci3310/curoom/MainActivity.java
+++ app/src/main/java/edu/cuhk/csci3310/curoom/MainActivity.java
@@ -9,13 +9,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -38,11 +49,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initially put random data into the image list, modify to pass correct info read from JSON
-        int num = (int) (Math.random()*6)+1;
-        for(int i=1; i<=num; i++) {
-            mImagePathList.addLast(mDrawableFilePath + "temp");
+        try {
+            String jsonData = readJsonFile(R.raw.cu_buildings);
+            Gson gson = new Gson();
+            JsonObject jsonObject = JsonParser.parseString(jsonData).getAsJsonObject();
+            JsonArray buildingsArray = jsonObject.getAsJsonArray("buildings");
+
+            for (JsonElement element : buildingsArray) {
+                JsonObject buildingObject = element.getAsJsonObject();
+                String picRoom = buildingObject.get("pic_room").getAsString();
+                Log.i("onCreate: 1111", picRoom);
+                String fileNameWithoutExtension = picRoom.substring(0, picRoom.lastIndexOf('.'));
+
+                String imagePath = mDrawableFilePath + fileNameWithoutExtension;
+                mImagePathList.addLast(imagePath);
+            }
+
+            // Now mImagePathList contains the file paths for the images
+            // You can use this list to display images or perform other operations
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
+
+        // Initially put random data into the image list, modify to pass correct info read from JSON
+//        int num = (int) (Math.random()*6)+1;
+//        for(int i=1; i<=num; i++) {
+//            mImagePathList.addLast(mDrawableFilePath + "temp");
+//        }
+
 
         // Get a handle to the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerview);
@@ -57,6 +93,21 @@ public class MainActivity extends AppCompatActivity {
         // TODO: Update the layout manager
         //  i.e. Set up Grid layout
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private String readJsonFile(int resourceId) throws IOException {
+        Resources resources = getResources();
+        InputStream inputStream = resources.openRawResource(resourceId);
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        StringBuilder builder = new StringBuilder();
+        char[] buffer = new char[1024];
+        int charsRead;
+
+        while ((charsRead = reader.read(buffer)) != -1) {
+            builder.append(buffer, 0, charsRead);
+        }
+
+        return builder.toString();
     }
 
     // TODO:
